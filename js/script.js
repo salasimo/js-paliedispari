707b56049533007b6e7ea7dabaa9a2695772cb64
Simone Sala <simonesala@me.com>
2020-02-24 17:32:27 +0000

    inseriti maggiori controlli in pari-dispari

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ if (risposta) {
 function isPalindrome(param) {
     var array = param.split("");
     for (var i = 0; i < array.length; i++) { //elimino gli spazi per rendere palindrome anche frasi come "i topi non avevano nipoti"
-        if (array[i] == " "){
+        if (array[i] == " ") {
             array.splice(i, 1)
         }
     }
@@ -62,34 +62,38 @@ function isPalindrome(param) {
 // }
 
 //ESERCIZIO RISOLTO CON UNA FUNZIONE====================
-
+var blocco = false;
 var sceltaPariDispari = prompt("Scegli pari o dispari.").toLowerCase();
-var numeroScelto = parseInt(prompt("Scegli un numero."));
-
-var risultatoGioco = giocaPariDispari(sceltaPariDispari, numeroScelto);
-if (risultatoGioco){
-console.log("Hai scelto", numeroScelto + ". Il PC ha estratto", numeroEstratto, "e il totale è", sommaTotale + ". YOU WIN!");
-} else{
-console.log("Hai scelto", numeroScelto + ". Il PC ha estratto", numeroEstratto, "e il totale è", sommaTotale + ". YOU LOSE :(")
+if (sceltaPariDispari != "pari" && sceltaPariDispari != "dispari") {
+    alert("Non hai scritto pari o dispari!");
+    blocco = true;
+} else {
+    var numeroScelto = parseInt(prompt("Scegli un numero."));
 }
 
-function giocaPariDispari(schieramento, numeroScelto){
+if (blocco == false) {
+    var risultatoGioco = giocaPariDispari(sceltaPariDispari, numeroScelto);
+    if (risultatoGioco) {
+        console.log("Hai scelto", numeroScelto + ". Il PC ha estratto", numeroEstratto, "e il totale è", sommaTotale + ". YOU WIN!");
+    } else {
+        console.log("Hai scelto", numeroScelto + ". Il PC ha estratto", numeroEstratto, "e il totale è", sommaTotale + ". YOU LOSE :(")
+    }
+}
+
+function giocaPariDispari(schieramento, numeroScelto) {
     numeroEstratto = Math.floor(Math.random() * (100 - 1)) + 1;
     sommaTotale = numeroScelto + numeroEstratto;
-    if (sommaTotale % 2 == 0){
+    if (sommaTotale % 2 == 0) {
         var check = "pari";
     } else {
         var check = "dispari";
     }
-    if (check == schieramento){
+    if (check == schieramento) {
         return true; //WIN
-    } else{
+    } else {
         return false; //LOSE
     }
 }
-
-
-
 
 
 
